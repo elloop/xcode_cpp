51a3f64b635253e659c3fc0c326375a7bcc1b938
elloop <elloop@foxmail.com>
2016-03-14 21:12:05 +0800

    king's cake

--- cpp/cpp/array_algo_test.cpp
+++ cpp/cpp/array_algo_test.cpp
@@ -1,22 +1,22 @@
-#include "array_algo_test.hpp"
-
-NS_BEGIN(elloop);
-NS_BEGIN(array_algo);
-
-void ArrayAlgo::run()
-{
-    remove_elems_by_indexes();
-}
-
-void ArrayAlgo::remove_elems_by_indexes()
-{
-    int indexes[] = {5, 3, 2, 8, 4};
-    int elems[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    // rough solution.
-    // there is no solution yet. tonight will be ok.
-    
-}
-
-NS_END(array_algo);
-NS_END(elloop);
-
+//#include "array_algo_test.hpp"
+//
+//NS_BEGIN(elloop);
+//NS_BEGIN(array_algo);
+//
+//void ArrayAlgo::run()
+//{
+//    remove_elems_by_indexes();
+//}
+//
+//void ArrayAlgo::remove_elems_by_indexes()
+//{
+//    int indexes[] = {5, 3, 2, 8, 4};
+//    int elems[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+//    // rough solution.
+//    // there is no solution yet. tonight will be ok.
+//    
+//}
+//
+//NS_END(array_algo);
+//NS_END(elloop);
+//
--- cpp/cpp/main.cpp
+++ cpp/cpp/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  cpp
-//
-//  Created by lina on 16/3/9.
-//  Copyright © 2016年 china. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int cut(int m, int n)
+{
+    if (m == n)
+    {
+        return 1;
+    }
+    else if (m < n)
+    {
+        return cut(m, n-m) + 1;
+    }
+    else
+    {
+        return cut(m-n, n) + 1;
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    
+    int t ;
+    cin >> t;
+    
+    int m,n;
+    while (t--)
+    {
+        cin >> m >> n;
+        cout << cut(m, n) << endl;
+    }
     return 0;
 }
