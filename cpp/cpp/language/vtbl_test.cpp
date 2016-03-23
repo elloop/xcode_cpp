656427434eac4dd7ae8fa1e8cd09fd73b1002b69
elloop <elloop@foxmail.com>
2016-03-23 21:55:37 +0800

    new perm method

--- cpp/cpp/language/vtbl_test.cpp
+++ cpp/cpp/language/vtbl_test.cpp
@@ -19,7 +19,39 @@ class B
 {
 };
 
-RUN_GTEST(VtblTest, basic, @@);
+
+class Base {
+public:
+    virtual void f() { cout << "Base::f" << endl; }
+    virtual void g() { cout << "Base::g" << endl; }
+    virtual void h() { cout << "Base::h" << endl; }
+
+};
+
+BEGIN_TEST(VtblTest, basic, @@);
+
+typedef void(*Fun)(void);
+
+
+Base b;
+
+Fun pFun = NULL, pFun1=NULL, pFun2 = NULL, pFun3 = NULL;
+int** pVtab = (int**)&b;
+int* v1 = (int*)pVtab[0][0];
+int *v2 = (int*)pVtab[0][1];
+int *v3 = (int*)pVtab[0][2];
+
+cout <<  v1 << endl;
+cout <<  v2 << endl;
+cout <<  v3 << endl;
+
+pFun1 = (Fun)v1;
+pFun1();
+pFun2 = (Fun)v2;
+pFun2();
+pFun3 = (Fun)v3;
+pFun3();
+
 
 psln(sizeof(A));
 psln(sizeof(B));
---
+++ cpp/cpp/perm/perm.cpp
@@ -0,0 +1,86 @@
+#include "inc.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+
+NS_BEGIN(elloop);
+NS_BEGIN(perm);
+
+using namespace std;
+
+// recursive perm.
+void perm_string(string& s, size_t begin, size_t end)
+{
+    if (begin == end) 
+    {
+        psln(s);
+    }
+    else
+    {
+        for (size_t i=begin; i<=end; ++i) 
+        {
+            swap(s[i], s[begin]);
+            perm_string(s, begin + 1, end);
+            swap(s[i], s[begin]);
+        }
+    }
+}
+
+string insertToString(string str, int pos,  char c) 
+{
+   string temp(str.substr(0, pos));
+   temp.append(1, c);
+   temp.append(str.substr(pos, -1));
+   return temp;
+}
+
+void new_perm()
+{
+    string src("abcdefghij");
+    vector<string> v {"a"};
+
+    for (int i=1; i<src.size(); ++i) 
+    {
+        // printContainer(v, "v: ");
+        vector<string> temp;
+        temp.swap(v);
+
+        // how many string in the vector
+        for ( auto & str: temp ) 
+        {
+            // how long a string is.
+            int length = str.size();
+            for (int j=0; j<length+1; ++j) 
+            {
+                v.push_back(insertToString(str, j, src[i]));
+            }
+        }
+    }
+
+    // printContainer(v, "v:");
+    psln(v.size());
+    for (auto & str : v) 
+    {
+        psln(str);
+    }
+}
+
+RUN_GTEST(Perm, t, @@);
+
+
+//clock_t begin = clock();
+//clock_t end;
+
+
+//string src("abcdefghij");
+//perm_string(src, 0, src.size() - 1);
+//end = clock();
+
+//double sec = (end - begin)  CLOCKS_PER_SEC;
+//psln(sec);
+
+END_TEST;
+
+NS_END(elloop);
+NS_END(perm);
